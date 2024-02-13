0e9191c6b888439a27a33c7f0a890abc3e68ae6a
ilya <ilya.prikhodko22@gmail.com>
2024-02-13 20:01:52 +0500

    add host

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var popup;
 
 const CLIENT_ID = "3c0d2422-e6d8-43c4-90e8-431f6cdd4a33"
 const SECRET_KEY = "cfH4RzKVSqou9Hyvsam3xUM1g9RSC13DTiN4nDTejWOnkI4bthso8EBxMJYLmiWS"
+const dolgosrochniy = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImNjMzJlOWY2ZWI5NGE1NWY5ODcwMzM0ZmQ3MTg0Yjc1OWJmZjhiZmMzMTFmOTgyY2M5NTA0MjliOTA5NDllOGVmZGE4YTEyZDE3NDM4NWRlIn0.eyJhdWQiOiIzYzBkMjQyMi1lNmQ4LTQzYzQtOTBlOC00MzFmNmNkZDRhMzMiLCJqdGkiOiJjYzMyZTlmNmViOTRhNTVmOTg3MDMzNGZkNzE4NGI3NTliZmY4YmZjMzExZjk4MmNjOTUwNDI5YjkwOTQ5ZThlZmRhOGExMmQxNzQzODVkZSIsImlhdCI6MTcwNzgzNTg1MSwibmJmIjoxNzA3ODM1ODUxLCJleHAiOjE3MTgyMzY4MDAsInN1YiI6IjEwNjY4NjA2IiwiZ3JhbnRfdHlwZSI6IiIsImFjY291bnRfaWQiOjMxNTY2MTkwLCJiYXNlX2RvbWFpbiI6ImFtb2NybS5ydSIsInZlcnNpb24iOjIsInNjb3BlcyI6WyJjcm0iLCJmaWxlcyIsImZpbGVzX2RlbGV0ZSIsIm5vdGlmaWNhdGlvbnMiLCJwdXNoX25vdGlmaWNhdGlvbnMiXSwiaGFzaF91dWlkIjoiMjM1YmUwNjctMjk2OS00ZGIwLThjOWQtMDVkNDI5OTNhYWQxIn0.r5dFFLIw4s6o3ga3gKgtRv6bn5Cnl8BGyFeFZHRqyNMcvjajRgP6wPxDnF9h7tnSjgFVIsjAJkZUMVyEAFFcdr3cOGvsSO6_WiPjjSCVxJ61XxpIx5FZUekZBaLJEfVCg3bCq6oqNH3Akj5l_klPzvAaEamrNYySo7IoT8KGC3utd_3GlRZt_yinrdtr4W9fo3YzSpfqGAmx9PuVG3UGmg6Kw1wmEPVo4PfbnWPQbZs_92doNvWMuJKwVJgcZ-0rEKFro_X4IKw98RahANLT5yGb0TVn_JKKZ8Rp0_yMsLh-SXwAjzPL9SLTHxaFze2UBlif8BgkM8tH6Gf-OiZnnQ'
 
 // auth();
 
@@ -49,6 +50,7 @@ async function getTokens() {
     }),
     headers: {
       'Content-Type': 'application/json',
+      "Host": 'ilyaprikhodko22.amocrm.ru'
     },
   });
 
