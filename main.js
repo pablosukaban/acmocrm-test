d2515f0a98ca98a357e9df8f6542c6ca0fb814e6
ilya <ilya.prikhodko22@gmail.com>
2024-02-13 19:47:34 +0500

    update main

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var popup;
 const CLIENT_ID = "3c0d2422-e6d8-43c4-90e8-431f6cdd4a33"
 const SECRET_KEY = "cfH4RzKVSqou9Hyvsam3xUM1g9RSC13DTiN4nDTejWOnkI4bthso8EBxMJYLmiWS"
 
-auth();
+// auth();
 
 // 1. Открывает окно предоставления доступов
 function auth() {
@@ -24,3 +24,37 @@ function updateAuthInfo(e) {
   // 4. Закрываем модальное окно
   popup.close();
 }
+
+// ====
+
+const url = new URL(window.location.href);
+const code = url.searchParams.get('code');
+
+const a = 'https://www.amocrm.ru/oauth2/access_token';
+
+async function getTokens() {
+  if (!code) {
+    auth()
+    return;
+  }
+
+  const response = await fetch(a, {
+    method: 'POST',
+    body: JSON.stringify({
+      client_id: CLIENT_ID,
+      client_secret: SECRET_KEY,
+      grant_type: 'authorization_code',
+      code,
+      redirect_uri: 'https://acmocrm-test.vercel.app',
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const json = await response.json();
+
+  console.log(json);
+}
+
+getTokens();
