3b6fdd750e321c2daac331ff5bee43e2cc2129be
ilya <ilya.prikhodko22@gmail.com>
2024-02-14 21:14:29 +0500

    button

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const CLIENT_ID = '3c0d2422-e6d8-43c4-90e8-431f6cdd4a33';
+const CLIENT_ID = "3c0d2422-e6d8-43c4-90e8-431f6cdd4a33";
 const SECRET_KEY =
-  'cfH4RzKVSqou9Hyvsam3xUM1g9RSC13DTiN4nDTejWOnkI4bthso8EBxMJYLmiWS';
+  "cfH4RzKVSqou9Hyvsam3xUM1g9RSC13DTiN4nDTejWOnkI4bthso8EBxMJYLmiWS";
 
 let access_token;
 let refresh_token;
@@ -9,17 +9,22 @@ let responseData;
 function auth() {
   window.open(
     `https://www.amocrm.ru/oauth?client_id=${CLIENT_ID}&state=${123}&mode=post_message`,
-    'Предоставить доступ',
-    'scrollbars, status, resizable, width=750, height=580'
+    "Предоставить доступ",
+    "scrollbars, status, resizable, width=750, height=580",
   );
 }
 
 // ===============
+const button = document.getElementById("app");
+
+button.addEventListener("click", () => {
+  getTokens();
+});
 
 const currentUrl = new URL(window.location.href);
-const code = currentUrl.searchParams.get('code');
+const code = currentUrl.searchParams.get("code");
 
-const userBaseUrl = 'https://ilyaprikhodko22.amocrm.ru';
+const userBaseUrl = "https://ilyaprikhodko22.amocrm.ru";
 const authUrl = `${userBaseUrl}/oauth2/access_token`;
 const leadsUrl = `${userBaseUrl}/api/v4/leads`;
 
@@ -30,17 +35,17 @@ async function getTokens() {
   }
 
   const response = await fetch(authUrl, {
-    method: 'POST',
+    method: "POST",
     body: JSON.stringify({
       client_id: CLIENT_ID,
       client_secret: SECRET_KEY,
-      grant_type: 'authorization_code',
+      grant_type: "authorization_code",
       code,
-      redirect_uri: 'https://acmocrm-test.vercel.app',
+      redirect_uri: "https://acmocrm-test.vercel.app",
     }),
     headers: {
-      'Content-Type': 'application/json',
-      Host: 'ilyaprikhodko22.amocrm.ru',
+      "Content-Type": "application/json",
+      Host: "ilyaprikhodko22.amocrm.ru",
     },
   });
 
@@ -58,46 +63,48 @@ async function getLeads() {
   }
 
   const response = await fetch(leadsUrl, {
-    method: 'GET',
+    method: "GET",
     headers: {
-      Authorization: 'Bearer ' + access_token,
+      Authorization: "Bearer " + access_token,
     },
   });
 
   const json = await response.json();
-  responseData = json
-  
-  createTableFromJSON()
+  responseData = json;
+
+  createTableFromJSON();
 }
 
-getTokens();
-
 function createTableFromJSON() {
-  if (!responseData && !responseData?._embedded?.leads && responseData?._embedded?.leads?.length === 0) {
-    document.getElementById('table-container').innerText = 'Данных нет';
+  if (
+    !responseData &&
+    !responseData?._embedded?.leads &&
+    responseData?._embedded?.leads?.length === 0
+  ) {
+    document.getElementById("table-container").innerText = "Данных нет";
   }
 
   const leads = responseData._embedded.leads; // Получаем массив leads
 
   // Создаем таблицу и тело таблицы
-  const table = document.createElement('table');
-  table.setAttribute('border', '1'); // Добавляем рамку таблицы для визуализации
-  const tableBody = document.createElement('tbody');
+  const table = document.createElement("table");
+  table.setAttribute("border", "1"); // Добавляем рамку таблицы для визуализации
+  const tableBody = document.createElement("tbody");
 
   // Создаем шапку таблицы
-  const headerRow = document.createElement('tr');
+  const headerRow = document.createElement("tr");
   const headers = [
-    'ID',
-    'Название',
-    'Цена',
-    'Ответственный',
-    'Статус',
-    'Pipeline ID',
-    'Создано',
-    'Обновлено',
+    "ID",
+    "Название",
+    "Цена",
+    "Ответственный",
+    "Статус",
+    "Pipeline ID",
+    "Создано",
+    "Обновлено",
   ];
   headers.forEach((headerText) => {
-    const headerCell = document.createElement('th');
+    const headerCell = document.createElement("th");
     headerCell.textContent = headerText;
     headerRow.appendChild(headerCell);
   });
@@ -105,7 +112,7 @@ function createTableFromJSON() {
 
   // Наполняем таблицу строками
   leads.forEach((lead) => {
-    const row = document.createElement('tr');
+    const row = document.createElement("tr");
     const rowData = [
       lead.id,
       lead.name,
@@ -117,7 +124,7 @@ function createTableFromJSON() {
       new Date(lead.updated_at * 1000).toLocaleString(),
     ];
     rowData.forEach((data) => {
-      const cell = document.createElement('td');
+      const cell = document.createElement("td");
       cell.textContent = data;
       row.appendChild(cell);
     });
@@ -126,5 +133,5 @@ function createTableFromJSON() {
 
   // Добавляем тело таблицы в таблицу и таблицу в контейнер
   table.appendChild(tableBody);
-  document.getElementById('table-container').appendChild(table);
+  document.getElementById("table-container").appendChild(table);
 }
